5292c3eae41e28986c1c5f24bb6b112a597ba2f4
Raony Guimaraes <raonyguimaraes@gmail.com>
2025-07-17 10:55:53 +0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,32 @@
 echo "Installing Azdu"
-curl -sf -L https://github.com/raonyguimaraes/azdu/releases/download/0.1/azdu > /usr/local/bin/azdu
+#curl -sf -L https://github.com/raonyguimaraes/azdu/releases/download/0.1/azdu > /usr/local/bin/azdu
+echo "#!/usr/bin/env python3
+
+import argparse
+import os
+
+parser = argparse.ArgumentParser()
+parser.add_argument("path")
+args = parser.parse_args()
+
+def azdu(start_path = '.'):
+    total_size = 0
+    for dirpath, dirnames, filenames in os.walk(start_path):
+        for f in filenames:
+            fp = os.path.join(dirpath, f)
+            # skip if it is symbolic link
+            if not os.path.islink(fp):
+                total_size += os.path.getsize(fp)
+    return total_size
+
+def bytes_to_human_readable(num, suffix='B'):
+    for unit in ['', 'K', 'M', 'G', 'T', 'P', 'E', 'Z']:
+        if abs(num) < 1024.0:
+            return f"{num:3.1f}{unit}{suffix}"
+        num /= 1024.0
+    return f"{num:.1f}Yi{suffix}"
+
+if __name__ == '__main__':
+    print(bytes_to_human_readable(azdu(args.path)))" > /usr/local/bin/azdu
+
 chmod +x /usr/local/bin/azdu
